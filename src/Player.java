2180552f32a5b70d407f3c0396f8d1eb32a8b171
bruckerm <bruckerm@WITPF0TKD82.wit.private>
2018-04-05 08:43:35 -0400

    Added a main class

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,13 @@
-
 public class Player {
-
+	
+	private String name;
+	
+	Player(String name){
+		this.name = name;
+	}
+	
+	public void win() {//Class for if the player wins the game.
+		System.out.printf("%s wins!%n", name);
+	}
+	
 }
